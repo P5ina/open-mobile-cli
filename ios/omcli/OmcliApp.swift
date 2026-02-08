a28c4abd4d221a64bbdf50ad4529cbdb81640f33
Timur Turatbekov <timatur242005@gmail.com>
2026-02-08 11:16:14 +0600

    Initial iOS project

---
+++ ios/omcli/OmcliApp.swift
@@ -0,0 +1,17 @@
+//
+//  omcliApp.swift
+//  omcli
+//
+//  Created by Timur Turatbekov on 08.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct OmcliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
